bb821e3de3f9a1c84571a55a8741eac48cebc1d0
Documenter.jl <documenter@juliadocs.github.io>
2024-08-21 09:12:37 +0000

    build based on fee4620

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BlockSparseMatrices","category":"page"},{"location":"#BlockSparseMatrices","page":"Home","title":"BlockSparseMatrices","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BlockSparseMatrices.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BlockSparseMatrices]","category":"page"}]
+}
